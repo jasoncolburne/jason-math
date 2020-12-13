71175ccbe9664ffe4fd411052246dc89ebec8fff
Jason Colburne <j.colburne@gmail.com>
2020-12-13 16:00:58 -0500

    first commit

---
+++ lib/jason/math.rb
@@ -0,0 +1,8 @@
+require "jason/math/version"
+
+module Jason
+  module Math
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/jason/math/version.rb
@@ -0,0 +1,5 @@
+module Jason
+  module Math
+    VERSION = "0.1.0"
+  end
+end
