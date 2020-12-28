063627786c01bd408dbf140cf9b5bc6ae0a0ae32
Jason Colburne <j.colburne@gmail.com>
2020-12-27 20:46:19 -0500

    add #root_as_continued_fraction

--- lib/jason/math/analysis.rb
+++ lib/jason/math/analysis.rb
@@ -12,6 +12,30 @@ module Jason
       
         sequence
       end
+
+      def self.root_as_continued_fraction(n)
+        result = []
+        subresult = []
+
+        limit = (n ** 0.5).to_i
+
+        m = 0
+        d = 1
+        a = limit
+        result << a
+
+        seen = Set[]
+        until seen.include?([m, d, a])
+          subresult << a unless seen.empty?
+          seen << [m, d, a]
+          m = d * a - m
+          d = (n - m * m) / d
+          a = (limit + m) / d
+        end
+
+        result << subresult
+        result
+      end
     end
   end
 end
--- spec/jason/math/analysis_spec.rb
+++ spec/jason/math/analysis_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe Jason::Math::Analysis do
       it { is_expected.to eq([13, 40, 20, 10, 5, 16, 8, 4, 2, 1]) }
     end
   end
+
+  context "#root_as_continued_fraction" do
+    subject { described_class.root_as_continued_fraction(n) }
+
+    context "for 13" do
+      let(:n) { 13 }
+      it { is_expected.to eq([3, [1, 1, 1, 1, 6]]) }
+    end
+
+    context "for 94" do
+      let(:n) { 94 }
+      it { is_expected.to eq([9, [1, 2, 3, 1, 1, 5, 1, 8, 1, 5, 1, 1, 3, 2, 1, 18]]) }
+    end
+  end
 end
