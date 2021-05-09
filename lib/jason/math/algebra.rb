2f9ad03f65c9f082e2460fb608fb913e6043b1b4
Jason Colburne <j.colburne@gmail.com>
2021-05-09 14:21:25 -0400

    rename discriminant

--- lib/jason/math/algebra.rb
+++ lib/jason/math/algebra.rb
@@ -4,10 +4,10 @@ module Jason
       def self.solve_quadratic(a, b, c)
         # we don't need to do anything special with the discriminant because
         # ruby privides complex numbers out of the box
-        constant = (b ** 2 - 4 * a * c) ** 0.5
+        discriminant = (b ** 2 - 4 * a * c) ** 0.5
         divisor = 2 * a
 
-        [(-b + constant) / divisor, (-b - constant) / divisor].uniq
+        [(-b + discriminant) / divisor, (-b - discriminant) / divisor].uniq
       end
     end
   end
