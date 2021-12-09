fc343daada4c6585fff932b45170af8c1ada3c77
Jason Colburne <j.colburne@gmail.com>
2021-12-09 10:24:15 -0500

    add hamming distance (#21)

--- lib/jason/math/cryptography.rb
+++ lib/jason/math/cryptography.rb
@@ -11,6 +11,11 @@ module Jason
           result
         end.join
       end
+
+      def self.hamming_distance(a, b)
+        raise "Cannot compute hamming distance if lengths differ" unless a.length == b.length
+        Jason::Math::Utility.xor(a, b).unpack('B*').first.count('1')
+      end
     end
   end
 end
--- spec/jason/math/cryptography_spec.rb
+++ spec/jason/math/cryptography_spec.rb
@@ -1,9 +1,17 @@
 RSpec.describe Jason::Math::Cryptography do
-  context "#xor_cipher" do
+  context "#xor_cipher cryptopals example" do
     subject { described_class.xor_cipher(plaintext, key) }
 
     let(:plaintext) { "Burning 'em, if you ain't quick and nimble" }
     let(:key) { "ICE" }
     it { is_expected.to eq('0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20'.hex_to_byte_string) }
+  end
+
+  context "#hamming_distance cryptopals example" do
+    subject { described_class.hamming_distance(a, b) }
+
+    let(:a) { 'this is a test'.b }
+    let(:b) { 'wokka wokka!!!'.b }
+    it { is_expected.to eq(37) }
   end
 end
