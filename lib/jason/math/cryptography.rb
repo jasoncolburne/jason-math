c403128b19c740542ffe12cbfb32dc62f30e5ac0
Jason Colburne <j.colburne@gmail.com>
2021-12-10 01:53:53 -0400

    refactor xor

--- lib/jason/math/cryptography.rb
+++ lib/jason/math/cryptography.rb
@@ -1,17 +1,9 @@
 require 'jason/math/cryptography/elliptic_curve'
+require 'jason/math/cryptography/exclusive_or'
 
 module Jason
   module Math
     module Cryptography
-      def self.xor_cipher(data, key)
-        key_characters = key.chars
-        data.chars.map do |character|
-          result = Jason::Math::Utility.xor(character, key_characters[0])
-          key_characters << key_characters.shift
-          result
-        end.join
-      end
-
       def self.hamming_distance(a, b)
         raise "Cannot compute hamming distance if lengths differ" unless a.length == b.length
         Jason::Math::Utility.xor(a, b).unpack('B*').first.count('1')
---
+++ lib/jason/math/cryptography/exclusive_or.rb
@@ -0,0 +1,16 @@
+module Jason
+  module Math
+    module Cryptography
+      module ExclusiveOr
+        def self.cipher(data, key)
+          key_characters = key.chars
+          data.chars.map do |character|
+            result = Jason::Math::Utility.xor(character, key_characters[0])
+            key_characters << key_characters.shift
+            result
+          end.join
+        end
+      end
+    end
+  end
+end
