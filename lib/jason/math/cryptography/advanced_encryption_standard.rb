a0833c573cf88b5b52aaaab03dfd35f9558a048a
Jason Colburne <j.colburne@gmail.com>
2021-12-11 04:59:13 -0400

    add aes decrypt

--- lib/jason/math/cryptography/advanced_encryption_standard.rb
+++ lib/jason/math/cryptography/advanced_encryption_standard.rb
@@ -61,9 +61,27 @@ module Jason
           0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16,
         ].freeze
 
+        INVERSE_S_BOX = [
+          0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb,
+          0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb,
+          0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e,
+          0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25,
+          0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92,
+          0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84,
+          0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06,
+          0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b,
+          0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73,
+          0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e,
+          0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b,
+          0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4,
+          0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f,
+          0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef,
+          0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61,
+          0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d,
+        ].freeze
+
         def initialize(mode, key, initialization_vector = nil)
           mode_details = MODE_DETAILS[mode]
-          @algorithm = mode
           @mode = mode_details[:mode]
           @bits = mode_details[:bits]
           @rounds = mode_details[:rounds]
@@ -108,11 +126,20 @@ module Jason
           end
         end
 
+        def decrypt(cipher_text)
+          case @mode
+          when :ecb
+            decrypt_ecb(cipher_text)
+          else
+            raise "Unsupported mode"
+          end
+        end
+
         private def encrypt_ecb(clear_text)
           length = clear_text.length
           iterations = length / 16 + 1
-
           cipher_text = "".b
+
           iterations.times do |i|
             to_cipher = i * 16 < length ? clear_text[(i * 16)..[(i + 1) * 16 - 1, length - 1].min] : "".b
             padding = 16 - to_cipher.length
@@ -121,6 +148,24 @@ module Jason
           end
 
           cipher_text
+        end
+
+        private def decrypt_ecb(cipher_text)
+          length = cipher_text.length
+
+          raise "Invalid cipher text length (must be a multiple of block size)" unless (length % 16).zero?
+
+          iterations = length / 16
+          clear_text = "".b
+
+          iterations.times do |i|
+            clear_text << decipher(cipher_text[(i * 16)..((i + 1) * 16 - 1)])
+          end
+
+          padding = clear_text.chars.last.ord
+          raise "Invalid padding, cannot decrypt" if padding > 16 || padding.zero?
+
+          clear_text[0..(length - padding - 1)]
         end
 
         private def cipher(clear_text)
@@ -137,9 +182,24 @@ module Jason
 
           state = sub_bytes(state)
           state = shift_rows(state)
-          state = add_round_key(state, @key_schedule[(@rounds * 16)..((@rounds + 1) * 16 - 1)])
+          add_round_key(state, @key_schedule[(@rounds * 16)..((@rounds + 1) * 16 - 1)])
+        end
 
-          state
+        private def decipher(cipher_text)
+          raise "Block ciphers cipher blocks with strict sizes (16 bytes for AES)" if cipher_text.length != 16
+
+          state = add_round_key(cipher_text, @key_schedule[(@rounds * 16)..((@rounds + 1) * 16 - 1)])
+
+          (@rounds - 1).downto(1) do |round|
+            state = inverse_shift_rows(state)
+            state = inverse_sub_bytes(state)
+            state = add_round_key(state, @key_schedule[(round * 16)..((round + 1) * 16 - 1)])
+            state = inverse_mix_columns(state)
+          end
+
+          state = inverse_shift_rows(state)
+          state = inverse_sub_bytes(state)
+          add_round_key(state, @key_schedule[0..15])
         end
 
         private def add_round_key(block, key_schedule_subset)
@@ -158,18 +218,45 @@ module Jason
           result
         end
 
+        private def inverse_shift_rows(block)
+          i = 0
+          result = "".b
+
+          16.times do
+            result << block[i]
+            i = (i - 3) % 16
+          end
+
+          result
+        end
+
         private def mix_columns(block)
           ranges = [0..3, 4..7, 8..11, 12..15]
           ranges.map { |range| mix_column(block[range]) }.join
         end
 
-        def mix_column(column)
+        private def inverse_mix_columns(block)
+          ranges = [0..3, 4..7, 8..11, 12..15]
+          ranges.map { |range| inverse_mix_column(block[range]) }.join
+        end
+
+        private def mix_column(column)
           bytes = column.bytes
           [
             galois_multiply(bytes[0], 2) ^ galois_multiply(bytes[1], 3) ^ galois_multiply(bytes[2], 1) ^ galois_multiply(bytes[3], 1),
             galois_multiply(bytes[0], 1) ^ galois_multiply(bytes[1], 2) ^ galois_multiply(bytes[2], 3) ^ galois_multiply(bytes[3], 1),
             galois_multiply(bytes[0], 1) ^ galois_multiply(bytes[1], 1) ^ galois_multiply(bytes[2], 2) ^ galois_multiply(bytes[3], 3),
             galois_multiply(bytes[0], 3) ^ galois_multiply(bytes[1], 1) ^ galois_multiply(bytes[2], 1) ^ galois_multiply(bytes[3], 2),
+          ].pack('C*')
+        end
+
+        private def inverse_mix_column(column)
+          bytes = column.bytes
+          [
+            galois_multiply(bytes[0], 0xe) ^ galois_multiply(bytes[1], 0xb) ^ galois_multiply(bytes[2], 0xd) ^ galois_multiply(bytes[3], 0x9),
+            galois_multiply(bytes[0], 0x9) ^ galois_multiply(bytes[1], 0xe) ^ galois_multiply(bytes[2], 0xb) ^ galois_multiply(bytes[3], 0xd),
+            galois_multiply(bytes[0], 0xd) ^ galois_multiply(bytes[1], 0x9) ^ galois_multiply(bytes[2], 0xe) ^ galois_multiply(bytes[3], 0xb),
+            galois_multiply(bytes[0], 0xb) ^ galois_multiply(bytes[1], 0xd) ^ galois_multiply(bytes[2], 0x9) ^ galois_multiply(bytes[3], 0xe),
           ].pack('C*')
         end
 
@@ -195,10 +282,18 @@ module Jason
           sub_bytes([word].pack('N*')).unpack('N*').first
         end
 
-        private def sub_bytes(bytes)
+        private def sub_bytes_core(bytes, box)
           result = "".b
-          bytes.bytes.each { |byte| result << S_BOX[byte].chr }
+          bytes.bytes.each { |byte| result << box[byte].chr }
           result
+        end
+
+        private def sub_bytes(bytes)
+          sub_bytes_core(bytes, S_BOX)
+        end
+
+        private def inverse_sub_bytes(bytes)
+          sub_bytes_core(bytes, INVERSE_S_BOX)
         end
 
         def decrypt_openssl(cipher_text)
@@ -218,8 +313,6 @@ module Jason
           clear_text = cipher.update(clear_text)
           clear_text + cipher.final
         end
-
-        alias_method :decrypt, :decrypt_openssl
       end
     end
   end
