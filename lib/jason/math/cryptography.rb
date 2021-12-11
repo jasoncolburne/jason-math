373900f9e4bd9c91e7815b2f8aca38ebd1773537
Jason Colburne <j.colburne@gmail.com>
2021-12-11 08:53:30 -0400

    add aes ofb mode

--- lib/jason/math/cryptography.rb
+++ lib/jason/math/cryptography.rb
@@ -12,42 +12,15 @@ module Jason
 
       class Cipher
         ALGORITHMS = {
-          aes_128_cbc: {
-            class: AdvancedEncryptionStandard,
-            mode: :cbc_128,
-          }.freeze,
-          aes_192_cbc: {
-            class: AdvancedEncryptionStandard,
-            mode: :cbc_192,
-          }.freeze,
-          aes_256_cbc: {
-            class: AdvancedEncryptionStandard,
-            mode: :cbc_256,
-          }.freeze,
-          aes_128_cfb: {
-            class: AdvancedEncryptionStandard,
-            mode: :cfb_128,
-          }.freeze,
-          aes_192_cfb: {
-            class: AdvancedEncryptionStandard,
-            mode: :cfb_192,
-          }.freeze,
-          aes_256_cfb: {
-            class: AdvancedEncryptionStandard,
-            mode: :cfb_256,
-          }.freeze,
-          aes_128_ecb: {
-            class: AdvancedEncryptionStandard,
-            mode: :ecb_128,
-          }.freeze,
-          aes_192_ecb: {
-            class: AdvancedEncryptionStandard,
-            mode: :ecb_192,
-          }.freeze,
-          aes_256_ecb: {
-            class: AdvancedEncryptionStandard,
-            mode: :ecb_256,
-          }.freeze,
+          aes_128_cbc: { class: AdvancedEncryptionStandard, mode: :cbc_128 }.freeze,
+          aes_192_cbc: { class: AdvancedEncryptionStandard, mode: :cbc_192 }.freeze,
+          aes_256_cbc: { class: AdvancedEncryptionStandard, mode: :cbc_256 }.freeze,
+          aes_128_cfb: { class: AdvancedEncryptionStandard, mode: :cfb_128 }.freeze,
+          aes_192_cfb: { class: AdvancedEncryptionStandard, mode: :cfb_192 }.freeze,
+          aes_256_cfb: { class: AdvancedEncryptionStandard, mode: :cfb_256 }.freeze,
+          aes_128_ecb: { class: AdvancedEncryptionStandard, mode: :ecb_128 }.freeze,
+          aes_192_ecb: { class: AdvancedEncryptionStandard, mode: :ecb_192 }.freeze,
+          aes_256_ecb: { class: AdvancedEncryptionStandard, mode: :ecb_256 }.freeze,
         }.freeze
 
         def initialize(algorithm, key, use_openssl = false)
