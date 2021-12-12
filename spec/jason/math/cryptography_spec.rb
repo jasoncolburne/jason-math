6291e8503f137a5c4dfe806f7d94a59c62745a69
Jason Colburne <j.colburne@gmail.com>
2021-12-12 09:10:29 -0400

    fix output of pkcs7

--- spec/jason/math/cryptography_spec.rb
+++ spec/jason/math/cryptography_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe Jason::Math::Cryptography do
     let(:b) { 'wokka wokka!!!'.b }
     it { is_expected.to eq(37) }
   end
+
+  context "#pad_pkcs7 cryptopals example" do
+    subject { described_class.pad_pkcs7(input, block_size) }
+
+    let(:input) { "YELLOW SUBMARINE" }
+    let(:block_size) { 20 }
+    it { is_expected.to eq("YELLOW SUBMARINE\x04\x04\x04\x04".b) }
+  end
 end
