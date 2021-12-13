65f6dd6c3ab1a7ef10490199c0a2171218755bc3
Jason Colburne <j.colburne@gmail.com>
2021-12-13 07:33:01 -0500

    add base64 convenience methods (#47)
    * add base64 convenience methods
    
    * fix

--- spec/jason/math/utility_spec.rb
+++ spec/jason/math/utility_spec.rb
@@ -87,7 +87,7 @@ RSpec.describe Jason::Math::Utility do
 
     context "cryptopals example" do
       let(:hex_string) { '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d' }
-      it { is_expected.to eq('SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t') }
+      it { is_expected.to eq("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hy\nb29t\n") }
     end
   end
 
