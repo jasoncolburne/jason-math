9c1fb6fe55d96d12f9c8ea57655bc3c5cc812a49
Jason Colburne <j.colburne@gmail.com>
2021-12-20 22:59:14 -0500

    move prng byte stream wrapper (#67)

--- lib/jason/math/cryptography.rb
+++ lib/jason/math/cryptography.rb
@@ -15,6 +15,28 @@ module Jason
         Utility.xor(a, b).unpack1('B*').count('1')
       end
 
+      # A generic bytestream constructed atop a PRNG
+      class PRNGByteStream
+        def initialize(prng, bytes_per_number)
+          @byte_stream = Enumerator.new do |yielder|
+            loop do
+              bytes = Utility.integer_to_byte_string(prng.extract_number).rjust(bytes_per_number, "\x00")
+              bytes.each_char do |char|
+                yielder << char
+              end
+            end
+          end
+        end
+
+        def take_byte
+          take_bytes
+        end
+
+        def take_bytes(count = 1)
+          @byte_stream.take(count).join
+        end
+      end
+
       # PKCS7 padding
       class PKCS7
         def self.pad(data, block_size)
