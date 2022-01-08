52e9b67f8d5bdb7909fd9795c056b0b0267dd7c3
Jason Colburne <j.colburne@gmail.com>
2022-01-08 06:06:58 -0500

    improve example client/server code (#111)

--- examples/client_server_crypto/server.rb
+++ examples/client_server_crypto/server.rb
@@ -7,22 +7,33 @@ require 'jason/math'
 require 'securerandom'
 require 'socket'
 
+Cryptography = Jason::Math::Cryptography
+
 server = TCPServer.new(ARGV[0] || 1337)
 curve = :secp384r1
+
+# we'll pretend we have a secure way of getting this key
+class SecretManager
+  def self.[](label)
+    raise "unexpected label" unless label == :server_signing_key
+
+    File.read('./server.key').b.byte_string_to_integer
+  end
+end
 
 loop do
   client = server.accept
 
-  ecc = Jason::Math::Cryptography::AsymmetricKey::EllipticCurve.new(curve)
+  ecc = Cryptography::AsymmetricKey::EllipticCurve.new(curve)
   my_private_ecdh_key = SecureRandom.random_bytes(48).byte_string_to_integer % ecc.n
   ecc.private_key = my_private_ecdh_key
   my_public_ecdh_key = ecc.generate_public_key!
   initialization_vector = SecureRandom.random_bytes(12) + "\x00" * 4
   payload = my_public_ecdh_key.to_byte_string(48) + initialization_vector
 
-  sha = Jason::Math::Cryptography::Digest::SecureHashAlgorithm.new(:'3_384')
+  sha = Cryptography::Digest::SecureHashAlgorithm.new(:'3_384')
   digest = sha.digest(payload).byte_string_to_integer
-  ecc.private_key = File.read('./server.key').b.byte_string_to_integer
+  ecc.private_key = SecretManager[:server_signing_key]
   signature = ecc.sign(
     digest, SecureRandom.random_bytes(48).byte_string_to_integer % ecc.n
   ).to_byte_string(48)
@@ -30,20 +41,25 @@ loop do
   client.write(payload.b + signature.b)
   data = client.recv(192)
 
-  signature = Jason::Math::Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(data[96..191])
+  signature = Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(data[96..191])
   digest = sha.digest(data[0..95]).byte_string_to_integer
-  ecc.public_key = Jason::Math::Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(File.read('./client.pub').b)
-  raise "could not verify client signature" unless ecc.verify(digest, signature)
+  ecc.public_key = Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(File.read('./client.pub').b)
+
+  if !ecc.verify(digest, signature)
+    puts "could not verify client signature"
+    exit(1)
+  end
+
   puts "verified client signature"
 
   puts "received public key: #{data[0..95].byte_string_to_hex}"
-  partner_public_ecdh_key = Jason::Math::Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(data[0..95])
+  partner_public_ecdh_key = Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(data[0..95])
   secret = ecc.compute_secret(my_private_ecdh_key, partner_public_ecdh_key)
   secret = (secret.x ^ secret.y).to_byte_string.rjust(48, "\x00")
   puts "computed shared secret: #{secret.byte_string_to_hex}"
   key = secret[0..23] ^ secret[24..47]
   puts "derived aes-gcm key: #{key.byte_string_to_hex}"
-  aes = Jason::Math::Cryptography::SymmetricKey::AdvancedEncryptionStandard.new(:gcm_192, key)
+  aes = Cryptography::SymmetricKey::AdvancedEncryptionStandard.new(:gcm_192, key)
   puts "using initialization vector: #{initialization_vector.byte_string_to_hex}"
   aes.initialization_vector = initialization_vector
 
