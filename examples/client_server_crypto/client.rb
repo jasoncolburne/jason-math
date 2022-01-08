fe97678316bc7c321f7443691122d0a71fbcf530
Jason Colburne <j.colburne@gmail.com>
2022-01-08 05:26:19 -0500

    improve client example (#110)

--- examples/client_server_crypto/client.rb
+++ examples/client_server_crypto/client.rb
@@ -1,15 +1,34 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
+require 'io/console'
 require 'rubygems'
 require 'bundler/setup'
 require 'jason/math'
 require 'securerandom'
 
+Cryptography = Jason::Math::Cryptography
+
+password = IO::console.getpass 'password: '
+data = File.read('./client.key').b
+
+salt = data[0..15]
+initialization_vector = data[16..31]
+ciphered_private_key = data[32..-17]
+tag = data[-16..]
+
+puts 'deriving storage key... (the password is "password")'
+# Note - these settings for Argon are not sufficient for real use, but the native implementation is slow
+argon = Cryptography::KeyStretching::Argon2.new(1, 32, 16384, 1)
+key = argon.stretch(password, salt)
+aes = Cryptography::SymmetricKey::AdvancedEncryptionStandard.new(:gcm_256, key)
+aes.initialization_vector = initialization_vector
+client_private_key = aes.decrypt(ciphered_private_key, '', tag).byte_string_to_integer
+
 socket = TCPSocket.new('0.0.0.0', ARGV.first || 1337)
 curve = :secp384r1
 
-ecc = Jason::Math::Cryptography::AsymmetricKey::EllipticCurve.new(curve)
+ecc = Cryptography::AsymmetricKey::EllipticCurve.new(curve)
 my_private_key = SecureRandom.random_bytes(48).byte_string_to_integer % ecc.n
 ecc.private_key = my_private_key
 my_public_key = ecc.generate_public_key!
@@ -18,18 +37,18 @@ data = socket.recv(96 + 16 + 96)
 payload = data[0..111]
 initialization_vector = data[96..111]
 
-signature = Jason::Math::Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(data[112..207])
-sha = Jason::Math::Cryptography::Digest::SecureHashAlgorithm.new(:'3_384')
+signature = Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(data[112..207])
+sha = Cryptography::Digest::SecureHashAlgorithm.new(:'3_384')
 digest = sha.digest(payload).byte_string_to_integer
-ecc.public_key = Jason::Math::Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(File.read('./server.pub').b)
+ecc.public_key = Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(File.read('./server.pub').b)
 raise "could not verify server signature" unless ecc.verify(digest, signature)
 puts "verified server signature"
 
 puts "received public key: #{data[0..95].byte_string_to_hex}"
-partner_public_key = Jason::Math::Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(data[0..95])
+partner_public_key = Cryptography::AsymmetricKey::EllipticCurve::Point.from_byte_string(data[0..95])
 payload = my_public_key.to_byte_string(48)
 digest = sha.digest(payload).byte_string_to_integer
-ecc.private_key = File.read('./client.key').b.byte_string_to_integer
+ecc.private_key = client_private_key
 signature = ecc.sign(digest, SecureRandom.random_bytes(48).byte_string_to_integer % ecc.n).to_byte_string(48)
 socket.write(payload + signature)
 
@@ -38,7 +57,7 @@ secret = (secret.x ^ secret.y).to_byte_string.rjust(48, "\x00")
 puts "computed shared secret: #{secret.byte_string_to_hex}"
 key = secret[0..23] ^ secret[24..47]
 puts "derived aes-gcm key: #{key.byte_string_to_hex}"
-aes = Jason::Math::Cryptography::SymmetricKey::AdvancedEncryptionStandard.new(:gcm_192, key)
+aes = Cryptography::SymmetricKey::AdvancedEncryptionStandard.new(:gcm_192, key)
 
 puts "using initialization vector: #{initialization_vector.byte_string_to_hex}"
 aes.initialization_vector = initialization_vector
