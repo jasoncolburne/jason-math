8407d6cc0dcb8cea0a72db28d1574462e9f3d7ec
Jason Colburne <j.colburne@gmail.com>
2022-01-10 00:25:18 -0400

    roots

--- lib/jason/math/algebra.rb
+++ lib/jason/math/algebra.rb
@@ -12,6 +12,28 @@ module Jason
 
         [(-b + discriminant_root) / divisor, (-b - discriminant_root) / divisor].uniq
       end
+
+      # finds y, the integer component of the nth root of n
+      # such that y ** n <= x < (y + 1) ** n
+      def self.root(x, n = 2)
+        high = 1
+
+        high *= 2 while high**n <= x
+        low = high / 2
+
+        while low < high
+          mid = (low + high) / 2
+          if low < mid && mid**n < x
+            low = mid
+          elsif high > mid && mid**n > x
+            high = mid
+          else
+            return mid
+          end
+        end
+
+        mid + 1
+      end
     end
   end
 end
